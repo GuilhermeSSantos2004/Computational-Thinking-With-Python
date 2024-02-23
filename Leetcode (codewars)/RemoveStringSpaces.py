fc09538c667f8ae9dfd920d7e631b60f9d10bdf3
Guilherme Santos <107642647+GuilhermeSSantos2004@users.noreply.github.com>
2024-02-23 20:02:06 -0300

    Create RemoveStringSpaces.py

---
+++ Leetcode (codewars)/RemoveStringSpaces.py
@@ -0,0 +1,6 @@
+def no_space(x):
+    frase_sem_espacos = ''.join(x.split())
+    return frase_sem_espacos
+
+# Divide a frase em palavras usando split() 
+# une as palavras sem espaços usando join()
