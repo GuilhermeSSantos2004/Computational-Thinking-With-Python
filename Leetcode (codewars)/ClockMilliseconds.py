5e098aebf7faa75eb01dfa4a75058924dcb6df91
Guilherme Santos <107642647+GuilhermeSSantos2004@users.noreply.github.com>
2024-02-26 20:20:31 -0300

    Update ClockMilliseconds.py

--- Leetcode (codewars)/ClockMilliseconds.py
+++ Leetcode (codewars)/ClockMilliseconds.py
@@ -1,3 +1,7 @@
-Clock shows h hours, m minutes and s seconds after midnight.
+# Clock shows h hours, m minutes and s seconds after midnight.
+# Your task is to write a function which returns the time since midnight in milliseconds.
 
-Your task is to write a function which returns the time since midnight in milliseconds.
+def time_in_milliseconds(h, m, s):
+    total_milliseconds = (h * 60 * 60 + m * 60 + s) * 1000
+    return total_milliseconds
+
